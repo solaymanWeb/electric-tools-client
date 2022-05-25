1e0c1de19dab40a313027424bbd299bf8a524d34
solayman1e <solaymanweb27@gmail.comm>
2022-05-26 04:03:38 +0600

    help section added

---
+++ src/Pages/Home/Help.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Help = () => {
+    return (
+<section className='py-16'>
+    <h4 className='font-bold text-2xl pb-12'>How can we help you ?</h4>
+<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+     <div class="card bg-gray-50 text-black">
+       <figure className=''>
+       <img className='w-16 h-16' src="https://cdn.makitatools.com/apps/wms/home/images/help/contact-us.png" alt="" />
+       </figure>
+        <div class="card-body items-center text-center">
+            <h2 class="card-title">CONTACT US</h2>
+            <p>Have a question? Need help?
+            Click here to contact us.</p>
+        </div>
+        </div>
+     <div class="card bg-gray-50 border-x-4">
+       <figure className=''>
+       <img className='w-16 h-16' src="https://cdn.makitatools.com/apps/wms/home//images/help/warranty.png" alt="" />
+       </figure>
+        <div class="card-body items-center text-center">
+            <h2 class="card-title">WARRANTY</h2>
+            <p>Questions about the warranty of your product?
+Learn more on our Warranty page.</p>
+        </div>
+        </div>
+     <div class="card bg-gray-50">
+       <figure className=''>
+       <img className='w-16 h-16' src="https://cdn.makitatools.com/apps/wms/home//images/help/service.png" alt="" />
+       </figure>
+        <div class="card-body items-center text-center">
+            <h2 class="card-title">SERVICE</h2>
+            <p>Have a tool that needs to be serviced?
+Weill help you get back to work.</p>
+        </div>
+        </div>
+     </div>
+</section>
+    );
+};
+
+export default Help;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import BusinessSummary from './BusinessSummary';
+import Help from './Help';
 import Offer from './Offer';
 import Review from './Review';
 import Services from './Services';
@@ -14,7 +15,7 @@ const Home = () => {
             <BusinessSummary></BusinessSummary>
             <Review></Review>
             <Offer></Offer>
-      
+            <Help></Help>
         </div>
     );
 };
