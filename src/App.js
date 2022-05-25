6d226a1ceaedf3e436e3049202f1f22417c90e34
solayman1e <solaymanweb27@gmail.comm>
2022-05-25 16:52:06 +0600

    menue bar complead

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Navbar from './Pages/Shared/Navbar';
+import Signup from './Pages/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      <h3>Welcome electric tools</h3>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home/>} ></Route>
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
+        
+
+      </Routes>
+      
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Home = () => {
+    return (
+        <div>
+            <h4>home</h4>
+        </div>
+    );
+};
+
+export default Home;
